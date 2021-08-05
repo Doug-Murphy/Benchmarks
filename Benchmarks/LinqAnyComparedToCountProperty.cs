7cf718ed8a8eaab98ef0aae761bdd1aec752aade
Doug Murphy <doug.murphy82@gmail.com>
2021-08-04 20:48:35 -0400

    Add benchmark comparing .Any() from System.Linq with .Count > 0.

---
+++ Benchmarks/LinqAnyComparedToCountProperty.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class LinqAnyComparedToCountProperty {
+        private readonly ImmutableList<int> _list = new List<int> { 1, 2, 3, 4, 5 }.ToImmutableList();
+
+        [Benchmark]
+        public void LinqAny() {
+            bool containsElements = _list.Any();
+        }
+
+        [Benchmark]
+        public void CountProperty() {
+            bool containsElements = _list.Count > 0;
+        }
+    }
+}
