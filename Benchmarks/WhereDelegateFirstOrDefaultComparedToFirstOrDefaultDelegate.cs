8e7bf6cbb7e95877becb1eb2095f3b6f3e72c07d
Doug Murphy <doug.murphy82@gmail.com>
2021-08-13 21:34:58 -0400

    Add benchmark for .Where(delegate).FirstOrDefault() vs .FirstOrDefault(delegate)

---
+++ Benchmarks/WhereDelegateFirstOrDefaultComparedToFirstOrDefaultDelegate.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class WhereDelegateFirstOrDefaultComparedToFirstOrDefaultDelegate {
+        private readonly ImmutableList<int> _list = Enumerable.Range(0, 1_000_000).ToImmutableList();
+
+        [Benchmark]
+        public int WhereDelegateFirstOrDefault() {
+            return _list.Where(x => x > 500_000).FirstOrDefault();
+        }
+
+        [Benchmark]
+        public int FirstOrDefaultDelegate() {
+            return _list.FirstOrDefault(x => x > 500_000);
+        }
+    }
+}
