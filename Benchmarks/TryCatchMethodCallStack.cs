cdabaad85227d42fd48746517801118c53ee8b36
Doug Murphy <doug.murphy82@gmail.com>
2021-08-17 21:21:51 -0400

    Add benchmarks for some different try/catch scenarios.

---
+++ Benchmarks/TryCatchMethodCallStack.cs
@@ -0,0 +1,96 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class TryCatchMethodCallStack {
+        [Benchmark]
+        public void TopLevelTryCatchOnly() {
+            try {
+                ChildLevel1WithoutTryCatch();
+            }
+            catch (Exception) {
+                //do nothing
+            }
+        }
+        private void ChildLevel1WithoutTryCatch() {
+            ChildLevel2WithoutTryCatch();
+        }
+        private void ChildLevel2WithoutTryCatch() {
+            ChildLevel3WithoutTryCatch();
+        }
+        private void ChildLevel3WithoutTryCatch() {
+            throw new Exception("Exception message.");
+        }
+
+
+        [Benchmark]
+        public void TopLevelAndChildLevelCatches() {
+            try {
+                ChildLevel1WithTryCatch();
+            }
+            catch (Exception) {
+                //do nothing
+            }
+        }
+        private void ChildLevel1WithTryCatch() {
+            try {
+                ChildLevel2WithTryCatch();
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+        private void ChildLevel2WithTryCatch() {
+            try {
+                ChildLevel3WithTryCatch();
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+        private void ChildLevel3WithTryCatch() {
+            try {
+                throw new Exception("Exception message.");
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+
+        [Benchmark]
+        public void TopLevelAndChildLevelCatchesWithThrowNew() {
+            try {
+                ChildLevel1WithTryCatchWithThrowNew();
+            }
+            catch (Exception) {
+                //do nothing
+            }
+        }
+        private void ChildLevel1WithTryCatchWithThrowNew() {
+            try {
+                ChildLevel2WithTryCatchWithThrowNew();
+            }
+            catch (Exception ex) {
+                throw new Exception("Exception container.", ex);
+            }
+        }
+        private void ChildLevel2WithTryCatchWithThrowNew() {
+            try {
+                ChildLevel3WithTryCatchWithThrowNew();
+            }
+            catch (Exception ex) {
+                throw new Exception("Exception container.", ex);
+            }
+        }
+        private void ChildLevel3WithTryCatchWithThrowNew() {
+            try {
+                throw new Exception("Exception message.");
+            }
+            catch (Exception ex) {
+                throw new Exception("Exception container.", ex); 
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ using Benchmarks.Benchmarks;
 namespace Benchmarks {
     public class Program {
         public static void Main(string[] args) {
-            BenchmarkRunner.Run<WhereDelegateFirstOrDefaultComparedToFirstOrDefaultDelegate>();
+            BenchmarkRunner.Run<TryCatchMethodCallStack>();
+            //BenchmarkRunner.Run<WhereDelegateFirstOrDefaultComparedToFirstOrDefaultDelegate>();
             //BenchmarkRunner.Run<LinqAnyComparedToCountProperty>();
             //BenchmarkRunner.Run<MultipleIfComparedToSwitch>();
             //BenchmarkRunner.Run<StringSplittingTechniques>();
