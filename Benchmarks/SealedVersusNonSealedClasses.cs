6a5b80bcad05eeb138938bf6afedb4e4601729e9
Doug Murphy <doug.murphy@abrigo.com>
2021-08-20 09:25:11 -0400

    Add sealed vs non sealed class referencing with virtual calls.

---
+++ Benchmarks/SealedVersusNonSealedClasses.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class SealedVersusNonSealedClasses {
+        private readonly SealedClass _sealedClass = new SealedClass();
+
+        private readonly NonSealedClass _nonSealedClass = new NonSealedClass();
+
+        [Benchmark]
+        public int SealedClassReferencing() {
+            return _sealedClass.Foo() + 1;
+        }
+
+        [Benchmark(Baseline = true)]
+        public int NonSealedClassReferencing() {
+            return _nonSealedClass.Foo() + 1;
+        }
+
+        private class BaseClass {
+            public virtual int Foo() {
+                return 123;
+            }
+        }
+
+        private sealed class SealedClass : BaseClass {
+            public override int Foo() {
+                return 456;
+            }
+        }
+
+        private class NonSealedClass : BaseClass {
+            public override int Foo() {
+                return 456;
+            }
+        }
+    }
+}
