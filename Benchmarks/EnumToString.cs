fe371e3581b5bca2744122467deb57b253e01279
Doug Murphy <doug.murphy82@gmail.com>
2021-08-19 20:45:27 -0400

    Add Enum.ToString() vs switch statement benchmark.

---
+++ Benchmarks/EnumToString.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class EnumToString {
+        [Benchmark]
+        public string EnumValueToString() {
+            return Animals.Dog.ToString();
+        }
+
+        [Benchmark]
+        public string EnumValueSwitchNameof() {
+            Animals enumToUse = Animals.Dog;
+            switch (enumToUse) {
+                case Animals.Dog:
+                    return nameof(Animals.Dog);
+                case Animals.Cat:
+                    return nameof(Animals.Cat);
+                case Animals.Fish:
+                    return nameof(Animals.Fish);
+                default:
+                    throw new Exception("Invalid enum option.");
+            }
+        }
+
+
+        private enum Animals {
+            Dog,
+            Cat,
+            Fish
+        }
+    }
+}
