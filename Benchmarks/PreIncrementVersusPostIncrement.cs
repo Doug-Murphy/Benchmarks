ee03cae8ff3565e82bc7612fe7a476bad648c6c2
Doug Murphy <doug.murphy82@gmail.com>
2021-08-21 20:04:17 -0400

    Add pre vs post increment for loop.

---
+++ Benchmarks/PreIncrementVersusPostIncrement.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class PreIncrementVersusPostIncrement {
+        private const int NUM_ITERATIONS = 1_000_000;
+
+        [Benchmark]
+        public void PreIncrement() {
+            int counter = 0;
+            for (int i = 0; i < NUM_ITERATIONS; ++i) {
+                counter = i;
+            }
+        }
+
+        [Benchmark]
+        public void PostIncrement() {
+            int counter = 0;
+            for (int i = 0; i < NUM_ITERATIONS; i++) {
+                counter = i;
+            }
+        }
+    }
+}
