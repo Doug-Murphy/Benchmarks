91025165444e821755ed244b77e9873c67bb36c5
Doug Murphy <doug.murphy@abrigo.com>
2021-08-23 16:05:50 -0400

    Add benchmark for comparing static vs instanced method calls.

---
+++ Benchmarks/StaticMethodsVersusInstancedMethods.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class StaticMethodsVersusInstancedMethods {
+        private readonly ClassWithMethods _classWithMethods = new ClassWithMethods();
+
+        [Benchmark]
+        public void InstancedMethodCall() {
+            _classWithMethods.InstancedMethod();
+        }
+
+        [Benchmark]
+        public void InstancedMethodCallWithInstantiation() {
+            new ClassWithMethods().InstancedMethod();
+        }
+
+        [Benchmark]
+        public void StaticMethodCall() {
+            ClassWithMethods.StaticMethod();
+        }
+
+        private class ClassWithMethods {
+            public int InstancedMethod() {
+                return 42;
+            }
+
+            public static int StaticMethod() {
+                return 42;
+            }
+        }
+    }
+}
