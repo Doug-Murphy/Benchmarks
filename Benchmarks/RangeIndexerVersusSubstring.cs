f8fc613d3c2e54444d4375456565ccad32c6a5e1
Doug Murphy <doug.murphy@abrigo.com>
2021-08-24 14:11:25 -0400

    Add benchmark to compare .Substring() with range indexer for .NET Core and .NET 5+.

---
+++ Benchmarks/RangeIndexerVersusSubstring.cs
@@ -0,0 +1,20 @@
+﻿#if !NETFRAMEWORK
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class RangeIndexerVersusSubstring {
+        private const string TEST_STRING = "This is a string.";
+
+        [Benchmark]
+        public string RangeIndexer() {
+            return TEST_STRING[TEST_STRING.LastIndexOf(" ")..];
+        }
+
+        [Benchmark(Baseline = true)]
+        public string Substring() {
+            return TEST_STRING.Substring(TEST_STRING.LastIndexOf(" "));
+        }
+    }
+}
+#endif
