07612064fa5a3fbc0da291dba9b5e1b549b7b6af
Doug Murphy <doug.murphy@abrigo.com>
2021-08-26 08:31:11 -0400

    Add StringBuilder vs string concatenation.

---
+++ Benchmarks/StringConcatenationVersusStringBuilder.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class StringConcatenationVersusStringBuilder {
+        private const int NUM_OF_ITERATIONS = 100;
+
+        [Benchmark]
+        public string StringBuilder() {
+            var stringToReturn = new StringBuilder();
+
+            for (var i = 0; i < NUM_OF_ITERATIONS; i++) {
+                stringToReturn.Append(i);
+            }
+
+            return stringToReturn.ToString();
+        }
+
+        [Benchmark]
+        public string StringConcatenation() {
+            string stringToReturn = null;
+
+            for (var i = 0; i < NUM_OF_ITERATIONS; i++) {
+                stringToReturn += i;
+            }
+
+            return stringToReturn;
+        }
+    }
+}
