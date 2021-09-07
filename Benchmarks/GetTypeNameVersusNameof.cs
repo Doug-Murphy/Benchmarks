9b45255023981732c8b6e9052c928a3659b73923
Doug Murphy <doug.murphy@abrigo.com>
2021-09-07 15:28:01 -0400

    Add benchmark comparing GetType().Name with nameof(Class).

---
+++ Benchmarks/GetTypeNameVersusNameof.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class GetTypeNameVersusNameof {
+        [Benchmark]
+        public string GetTypeWithName() {
+            return GetType().Name;
+        }
+
+        [Benchmark(Baseline = true)]
+        public string NameofClass() {
+            return nameof(GetTypeNameVersusNameof);
+        }
+    }
+}
