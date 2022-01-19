71bafa74c1365c0815ade678712080d603c90621
Doug Murphy <doug.murphy@abrigo.com>
2022-01-19 11:04:16 -0500

    Add benchmark comparing various ways of checking if an enumerable contains an item.

---
+++ Benchmarks/LinqAnyVersusEnumerableContains.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class LinqAnyVersusEnumerableContains {
+        private readonly List<string> _list10000Items = Enumerable.Repeat("foo", 10_000).ToList();
+
+        [Benchmark]
+        public bool EnumerableContains() {
+            return Enumerable.Contains(_list10000Items, "foo");
+        }
+
+        [Benchmark]
+        public bool ListContains() {
+            return _list10000Items.Contains("foo");
+        }
+
+        [Benchmark]
+        public bool LinqAny() {
+            return _list10000Items.Any(x => x == "foo");
+        }
+
+        [Benchmark]
+        public bool ForeachEquals() {
+            foreach (string item in _list10000Items) {
+                if (Equals("foo", item)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
