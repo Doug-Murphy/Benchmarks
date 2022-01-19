a2ccfc36c959af9e717c22a5566a0e3d6635f9e3
Doug Murphy <doug.murphy82@gmail.com>
2022-01-19 13:46:45 -0500

    Add benchmark for finding an item in a list using list.Contains vs foreach.

---
+++ Benchmarks/ValueIsInList.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class ValueIsInList {
+        private static readonly List<string> fooList = Enumerable.Repeat("foo", 5_000).ToList();
+        private static readonly List<string> barList = Enumerable.Repeat("bar", 5_000).ToList();
+        private readonly List<string> list = fooList.Concat(barList).ToList();
+
+        [Params("foo", "bar")]
+        public string value;
+
+        [Benchmark(Baseline = true)]
+        public bool InBefore() {
+            if (list == null || list.Count == 0) {
+                return false;
+            }
+            return list.Contains(value);
+        }
+
+        [Benchmark]
+        public bool InAfter() {
+            if (list == null || list.Count == 0) {
+                return false;
+            }
+
+            foreach (string item in list) {
+                if (item.Equals(value)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
