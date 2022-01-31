c8c2d8293b544177420eb117be86d635f9160cf6
Doug Murphy <doug.murphy82@gmail.com>
2022-01-31 09:30:39 -0500

    Add benchmark comparing string.Empty with ""

---
+++ Benchmarks/StringEmptyVersusEmptyQuotes.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks {
+    [MemoryDiagnoser]
+    public class StringEmptyVersusEmptyQuotes {
+        [Benchmark]
+        public void StringEmpty() {
+            var foo = string.Empty;
+            string bar = foo + "bar";
+        }
+
+        [Benchmark]
+        public void EmptyQuotes() {
+            var foo = "";
+            string bar = foo + "bar";
+        }
+    }
+}
