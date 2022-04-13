96e6934b04a1f02460ab5bbe6c112b0a9d1e6d2a
Doug Murphy <doug.murphy82@gmail.com>
2022-04-13 09:40:54 -0400

    Remove .NET 5 support.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Benchmarks {
     public class Program {
         public static void Main(string[] args) {
             //Run with the following command on CLI to run a chosen benchmark on all runtimes after the --runtimes flag
-            //dotnet run -c Release --framework net48 --runtimes net48 netcoreapp3.1 net5.0 net6.0
+            //dotnet run -c Release --framework net48 --runtimes net48 netcoreapp3.1 net6.0
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
